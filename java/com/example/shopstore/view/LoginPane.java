46016fbb6fc4593d0c0bc6f4b29a9ca1f5ca1587
ceaserborgia <ceaserborgian@gmail.com>
2019-08-17 13:40:24 +0800

    wait for menage

--- java/com/example/shopstore/view/LoginPane.java
+++ java/com/example/shopstore/view/LoginPane.java
@@ -2,6 +2,7 @@ package com.example.shopstore.view;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -10,9 +11,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.shopstore.R;
 
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class LoginPane extends AppCompatActivity {
     Button btn_login;
     TextView tv_register;
+
+    ResultSet rst;
+    Connection conn;
+    PreparedStatement pst;
+
+
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -34,10 +48,35 @@ public class LoginPane extends AppCompatActivity {
         btn_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent=new Intent(LoginPane.this, HomePage.class);
-                startActivity(intent);
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Log.e("MYTAG", "开始尝试连接");
+                        testJdbc();
+                    }
+                }).start();
             }
         });
+    }
+
+    public void testJdbc(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            Log.e("MYTAG", "class加载完成");
+            conn= DriverManager.getConnection("jdbc:mysql://47.106.177.200:3306/store?serverTimezone=UTC&useUnicode=true&characterEncoding=utf8","ceaser","Q201023aaa");
+            Log.e("MYTAG", "Connection加载完成");
+            pst=conn.prepareStatement("use store");
+            Log.e("MYTAG", "pst处理完成");
+            rst=pst.executeQuery("select name from user where id=1");
+            Log.e("MYTAG", "查询处理完成");
+
+            while (rst.next()){
+                Log.i("SUCCESS", "testJdbc: name= "+String.valueOf(rst.getObject(1)));
+            }
+        }catch (ClassNotFoundException | SQLException e){
+            Log.e("ERROR", "testJdbc: "+e.getMessage() );
+        }finally {
+        }
     }
 
 }
